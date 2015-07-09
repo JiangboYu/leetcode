238381f67988f2f72061f358137baaf4d51a542a
Yu Jiangbo <alfred@r-120-97-25-172.comp.nus.edu.sg>
2015-07-09 15:48:45 +0800

    Solution accepted

---
+++ GenerateParenthesis.py
@@ -0,0 +1,27 @@
+class Solution1:
+    def generateParenthesis(self, n):
+        def generate(p, left, right, paren = []):
+            if left: generate( p + '(', left - 1, right, paren)
+            if right > left: generate(p + ')', left, right - 1, paren)
+            if not right: paren += p,
+            return paren
+            
+class Solution2:
+    def Parenthesis(self, list_Parenthesis, candidate, left, right):
+        if right == 0:
+            list_Parenthesis.append(candidate)
+            return
+        if left < right:
+            self.Parenthesis(list_Parenthesis, candidate + ')', left, right - 1)
+        if left > 0:
+            self.Parenthesis(list_Parenthesis, candidate + '(', left - 1, right)
+    # @return {string[]}
+    def generateParenthesis(self, n):
+        list_Parenthesis = []
+        candidate = ''
+        self.Parenthesis(list_Parenthesis, candidate,  n, n)
+        return list_Parenthesis
+if __name__ == "__main__":    
+    a = Solution1()
+    print a.generateParenthesis(3)
+            
