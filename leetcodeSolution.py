1d6dda562e925f464df904f068badf4dc62df306
Yu Jiangbo <alfred@Yus-MacBook-Pro-2.local>
2015-07-10 20:50:52 +0800

    Solution for maxProduct added

--- leetcodeSolution.py
+++ leetcodeSolution.py
@@ -1,4 +1,25 @@
 class Solution:
+    # @descrip Problem: Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+    #For example, given the array [2,3,-2,4],
+    #the contiguous subarray [2,3] has the largest product = 6.
+
+    # @param {integer[]} nums
+    # @return {integer}
+    def maxProduct(self, nums):
+        #Dynamic solution
+        #For each state, knowledge need to be maintained including
+        #  1. current max_p
+        #  2. current min_p
+        # max_p would tract the max product when num > 0
+        # min_p would tract the max product when num < 0
+        max_p, min_p, max_product = nums[0], nums[0], nums[0]
+        for num in nums[1:]:
+            tmp = max_p
+            max_p = max(max_p * num, num, min_p * num)
+            min_p = min(tmp * num, num, min_p * num)
+            max_product = max(max_product, max_p)
+        return max_product
+        
     def generateParenthesis(self, n):
         def generate(p, left, right, paren = []):
             if left: generate( p + '(', left - 1, right, paren)
@@ -22,5 +43,6 @@ class Solution:
         return list_Parenthesis
 if __name__ == "__main__":    
     a = Solution()
-    print a.generateParenthesis(3)
+    print a.maxProduct([2,3,-2,4])
+    #print a.generateParenthesis(3)
             
